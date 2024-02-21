f6b08cae1620f272f9be36c846548ef00f71e050
bifrost343 <yayodcv@gmail.com>
2024-02-21 16:42:22 -0600

    Extract server component<Header/> from layout,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Link from 'next/link';
+
+// components
+import { Header } from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,18 +19,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<header>
-					<nav>
-						<ul>
-							<Link href="/">
-								<li>Home</li>
-							</Link>
-							<Link href="/store">
-								<li>Store</li>
-							</Link>
-						</ul>
-					</nav>
-				</header>
+				<Header />
+				{children}
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 export default function Home() {
+  console.log('Hola mundo pagina de inicio')
   return (
     <main>
       <h1>Welcome</h1>
