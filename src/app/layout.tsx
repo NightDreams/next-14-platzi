f4238f910fc51d67cc652ed154267b8b39c99f8b
bifrost343 <yayodcv@gmail.com>
2024-03-04 10:36:13 -0600

    Next Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import './globals.css';
 
 // components
@@ -7,7 +7,10 @@ import { Header } from '@/components/shared/Header';
 import { Footer } from '@/components/shared/Footer';
 import '../sass/globals.sass';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+	weight: ['100', '300', '500', '700'],
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
 	title: 'Future World',
@@ -20,7 +23,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={roboto.className}>
 				<Header />
 				{children}
 				<Footer />
