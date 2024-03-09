105712db291b99bc23cd0760320deac0d88954e7
bifrost343 <yayodcv@gmail.com>
2024-03-08 19:17:55 -0600

    services arch

--- src/components/Home/MainProducts/MainProducts.tsx
+++ src/components/Home/MainProducts/MainProducts.tsx
@@ -1,22 +1,6 @@
 import Image from 'next/image';
 import styles from './MainProducts.module.sass';
-
-const getProducts = async () => {
-	try {
-		const response = await fetch(
-			`${process.env.SHOPIFY_HOSTNAME}admin/api/2023-10/products.json`,
-			{
-				headers: new Headers({
-					'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
-				}),
-			}
-		);
-		const { products } = await response.json();
-		return products;
-	} catch (error) {
-		console.log('🚀 ~ getProducts ~ error:', error);
-	}
-};
+import { getProducts } from '@/services/shopify';
 
 interface MainProductsProps {}
 
---
+++ src/services/shopify/index.ts
@@ -0,0 +1,19 @@
+// fetchfn snippet
+
+import { env } from '@/config/env';
+import { shopifyUrls } from './url';
+
+export const getProducts = async () => {
+	try {
+		const response = await fetch(shopifyUrls.products.all, {
+			headers: new Headers({
+				'X-Shopify-Access-Token': env.SHOPIFY_TOKEN,
+			}),
+		});
+
+		const { products } = await response.json();
+		return products;
+	} catch (error) {
+		console.log('🚀 ~ getProducts ~ error:', error);
+	}
+};
