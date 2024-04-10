e7bde40da6ed983f995e4f2f22f75394b5b8745d
bifrost343 <yayodcv@gmail.com>
2024-04-10 15:24:05 -0600

    style /store categories

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,6 +1,7 @@
 import { getCollections } from '@/services/shopify/collections';
 import Link from 'next/link';
 
+import styles from './StoreLayout.module.sass';
 export default async function Layout({
 	children,
 }: {
@@ -9,11 +10,16 @@ export default async function Layout({
 	const collections = await getCollections();
 
 	return (
-		<main>
+		<main className={styles.StoreLayout}>
+			<h1>Explore </h1>
 			<nav>
-				<ul>
+				<ul className={styles.StoreLayout__list}>
 					{collections.map(collection => (
-						<Link key={collection.id} href={'/store/' + collection.handle}>
+						<Link
+							key={collection.id}
+							href={'/store/' + collection.handle}
+							className={styles.StoreLayout__chip}
+						>
 							{collection.title}
 						</Link>
 					))}
