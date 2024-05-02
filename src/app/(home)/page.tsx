a9e98da5c195f13fca0f5562f9d5ac81addc75a4
bifrost343 <yayodcv@gmail.com>
2024-05-02 09:54:53 -0600

    Seo - Dinamic Metadata, Product/[handle]

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,12 @@
 // components
 import { MainProducts } from '@/components/Home/MainProducts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: '✨ Future world',
+	description: 'Welcome to the future world, an ecommerce from other century',
+	keywords: ['ecommerce', 'future', 'world', 'technology'],
+};
 
 export default function Home() {
 	return (
