0ca875ac06f5cafa08beabf83b8ccc608fc86911
bifrost343 <yayodcv@gmail.com>
2024-05-08 18:04:02 -0600

    server action - handleCreateUser / Signup

--- src/components/singup/NewAccountForm/NewAccountForm.tsx
+++ src/components/singup/NewAccountForm/NewAccountForm.tsx
@@ -1,16 +1,23 @@
 'use client';
 
-import { useState } from 'react';
+import { handleCreateUser } from '@/actions';
+import { DetailedHTMLProps, FormHTMLAttributes, useState } from 'react';
 import styles from './NewAccountForm.module.sass';
 
 export const NewAccountForm = () => {
 	const [errors, setErrors] = useState<string[]>([]);
 	const [loading, setLoading] = useState<boolean>(false);
 
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		handleCreateUser(formData);
+	};
+
 	return (
 		<div className={styles.NewAccountForm}>
 			<h1 className={styles.NewAccountForm__title}>New Account</h1>
-			<form className={styles.NewAccountForm__form}>
+			<form onSubmit={handleSubmit} className={styles.NewAccountForm__form}>
 				<input
 					type="text"
 					name="first_name"
