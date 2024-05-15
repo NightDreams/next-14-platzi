9923abd8a17058a8d05b10ecd7b7bb5155aea5e5
bifrost343 <yayodcv@gmail.com>
2024-05-15 12:15:07 -0600

    feat: adding access-cookie. + utils - crateAccessToken , implement in handleCreateUser and type queries.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,33 @@
 'use server';
-import { GraphQLClientSingleton } from 'app/graphql';
-import { createUserMutation } from 'app/graphql/mutations/createUserMutation';
+import { GraphQLClientSingleton } from '@/graphql';
+import { createUserMutation } from '@/graphql/mutations/createUserMutation';
+
+import { createAccessToken } from '@/utils/auth/createAccessToken';
+import { Enum } from 'graphql-request/alpha/schema';
+import { redirect } from 'next/navigation';
+
+export interface TCustomerCreate {
+	customerCreate: CustomerCreate;
+}
+
+export interface CustomerCreate {
+	customer: Customer;
+	customerUserErrors: CustomerUserError;
+}
+
+export interface CustomerUserError {
+	code: string;
+	field: string;
+	message: string;
+}
+
+export interface Customer {
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	acceptsMarketing: boolean;
+}
 
 export const handleCreateUser = async (formData: FormData) => {
 	const formDataObject = Object.fromEntries(formData);
@@ -16,11 +43,17 @@ export const handleCreateUser = async (formData: FormData) => {
 		},
 	};
 
-	const { customerCreate } = await graphqlClient.request(
+	const { customerCreate } = await graphqlClient.request<TCustomerCreate>(
 		createUserMutation,
 		variables
 	);
+	// log data
 	const { customerUserErrors, customer } = customerCreate;
-	console.log(customer);
-	console.log(customerUserErrors);
+	if (!customer) {
+		await createAccessToken(
+			formDataObject.email as string,
+			formDataObject.password as string
+		);
+		redirect('/store');
+	}
 };
---
+++ src/utils/auth/createAccessToken.ts
@@ -0,0 +1,47 @@
+import { GraphQLClientSingleton } from '@/graphql';
+import { customerAccessTokenCreateMutation } from '@/graphql/mutations/customerAcessTokenCreate';
+import { cookies } from 'next/headers';
+
+export interface AccessToken {
+	customerAccessTokenCreate: {
+		customerAccessToken: CustomerAccessToken;
+		customerUserErrors: any[];
+	};
+}
+
+export interface CustomerAccessTokenCreate {
+	customerAccessToken: CustomerAccessToken;
+	customerUserErrors: any[];
+}
+
+export interface CustomerAccessToken {
+	accessToken: string;
+	expiresAt: Date;
+}
+
+export const createAccessToken = async (email: string, password: string) => {
+	const cookiesStore = cookies();
+	const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+
+	// mutation
+	const { customerAccessTokenCreate } =
+		await graphqlClient.request<AccessToken>(
+			customerAccessTokenCreateMutation,
+			{
+				email: email,
+				password: password,
+			}
+		);
+
+	const { accessToken, expiresAt } =
+		customerAccessTokenCreate?.customerAccessToken;
+
+	if (accessToken) {
+		cookiesStore.set('accessToken', accessToken, {
+			path: '/',
+			expires: new Date(expiresAt),
+			httpOnly: true,
+			sameSite: 'strict',
+		});
+	}
+};
