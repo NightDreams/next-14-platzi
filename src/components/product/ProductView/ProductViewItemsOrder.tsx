f4734179835be4e2e3dbcad0daa91bd17f177741
bifrost343 <yayodcv@gmail.com>
2024-05-17 20:40:18 -0600

    implement zustand / adding cart buy

--- src/components/product/ProductView/ProductViewItemsOrder.tsx
+++ src/components/product/ProductView/ProductViewItemsOrder.tsx
@@ -2,16 +2,30 @@
 import { SyntheticEvent, useState } from 'react';
 import { FaCartShopping } from 'react-icons/fa6';
 import styles from './ProductViewItemsOrder.module.sass';
+import { useShoppingCart } from '@/hooks/useShoppingCart';
 
 interface ProductViewItemsOrderProps {
 	maxQuantity: number;
+	product: ProductType;
 }
 
 export const ProductViewItemsOrder = ({
 	maxQuantity,
+	product,
 }: ProductViewItemsOrderProps) => {
 	const [counter, setCounter] = useState(1);
+	const { addToCart } = useShoppingCart();
+	const { title, id, price, quantity } = product;
 
+	const handleAddToCart = (event: SyntheticEvent) => {
+		event.preventDefault();
+		addToCart({
+			title,
+			id,
+			price,
+			quantity,
+		});
+	};
 	const handleSubmit = (event: SyntheticEvent) => {
 		event.preventDefault();
 	};
@@ -39,7 +53,11 @@ export const ProductViewItemsOrder = ({
 				onSubmit={handleSubmit}
 				className={styles.ProductViewItemsOrder__form}
 			>
-				<button className={styles.ProductViewItemsOrder__submit} type="submit">
+				<button
+					className={styles.ProductViewItemsOrder__submit}
+					type="submit"
+					onClick={handleAddToCart}
+				>
 					<FaCartShopping />
 					<span>Add to cart</span>
 				</button>
