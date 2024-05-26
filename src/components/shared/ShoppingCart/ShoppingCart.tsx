a1ee5677b45d6e5c308d49b4c25afbbec75e9b23
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-05-26 13:41:37 -0600

    feat: adding cart items display

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -6,11 +6,24 @@ import { useShoppingCart } from '@/hooks/useShoppingCart';
 
 export const ShoppingCart = () => {
 	const { cart } = useShoppingCart();
+	const [isOpen, setIsOpen] = useState(false);
+	const handleOpen = () => setIsOpen(!isOpen);
 
 	return (
-		<button className={styles.ShoppingCart}>
+		<button className={styles.ShoppingCart} onClick={handleOpen}>
 			<span className={styles.ShoppingCart__counter}> {cart.length}</span>
 			<FaShoppingCart />
+			{isOpen && (
+				<div className={styles.ShoppingCart__items}>
+					{cart.map(item => (
+						<>
+							<p key={item?.id}>{item?.title}</p>
+							<p>Cantidad: {item.quantity}</p>
+						</>
+					))}
+					<button className={styles.ShoppingCart__buyButton}>Buy</button>
+				</div>
+			)}
 		</button>
 	);
 };
